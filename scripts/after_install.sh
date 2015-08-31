1e7f4ed7c931d5222be186573e0aefaf6e1209f5
silencecork <silencecork@silencecork.com>
2015-08-31 13:41:45 +0800

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 chmod 777 /home/ec2-user/server -R
 cd /home/ec2-user/server
-node --help
+if ! rpm -qa | grep nodejs; then
+	curl --silent --location https://rpm.nodesource.com/setup | bash -
+	yum -y install nodejs 
+fi
 npm install
